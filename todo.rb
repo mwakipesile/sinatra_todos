646a36f59f0ddb466bd9df66b55ee80acbc013ac
M. Edgar joel <mkakajoel@gmail.com>
2016-09-18 17:37:39 -0400

    Added functionality: create new list

--- todo.rb
+++ todo.rb
@@ -1,7 +1,34 @@
 require "sinatra"
-require "sinatra/reloader"
+require "sinatra/reloader" if development?
 require "tilt/erubis"
 
-get "/" do
-  erb "You have no lists.", layout: :layout
+configure do
+  enable :sessions
+  set :session_secret, 'password1'
+end
+
+before do 
+  session[:lists] ||= []
+  @lists = session[:lists]
+end
+
+get '/' do
+  redirect('/lists')
+end
+
+get '/lists' do
+  erb :lists, layout: :layout
+end
+
+get '/lists/new' do
+  erb :new_list, layout: :layout
+end
+
+post '/lists' do
+  session[:lists] << { name: params['list_name'], todos: [] }
+  redirect('/lists')
+end
+get '/lists/:id' do |id|
+  @list_id = id.to_i
+  erb :list, layout: :layout
 end
