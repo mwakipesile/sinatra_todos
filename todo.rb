72ee6d8f4a3e6ece0604dfc46df58cb7e79bea0f
M. Edgar joel <mkakajoel@gmail.com>
2016-09-19 16:14:50 -0400

    Added list creation success flash message

--- todo.rb
+++ todo.rb
@@ -16,18 +16,24 @@ get '/' do
   redirect('/lists')
 end
 
+# View a list of all lists
 get '/lists' do
   erb :lists, layout: :layout
 end
 
+# Render the new list form
 get '/lists/new' do
   erb :new_list, layout: :layout
 end
 
+# Create a new list
 post '/lists' do
-  session[:lists] << { name: params['list_name'], todos: [] }
+  list_name = params['list_name']
+  session[:lists] << { name: list_name, todos: [] }
+  session[:success] = "#{list_name} list created!"
   redirect('/lists')
 end
+
 get '/lists/:id' do |id|
   @list_id = id.to_i
   erb :list, layout: :layout
